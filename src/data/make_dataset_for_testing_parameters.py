de812d8a265b92b1b3685725bb1f2755bf098817
Arbona <jeanmichel.arbona@gmail.com>
2017-02-02 13:51:36 +0100

    added simulation

--- src/data/make_dataset_for_testing_parameters.py
+++ src/data/make_dataset_for_testing_parameters.py
@@ -6,5 +6,10 @@ import os
 if __name__ == "__main__":
     param_file = sys.argv[1]
     parameters = load_parameters(param_file)
-    parameters["data_folder"] = os.path.join(parameters["data_folder"],
-                                             parameters["sumatra_label"])
+
+    if "sumatra_label" in parameters:
+        parameters["data_folder"] = os.path.join(parameters["data_folder"],
+                                                 parameters["sumatra_label"])
+    else:
+        print("no extra label")
+    simulate(parameters)
