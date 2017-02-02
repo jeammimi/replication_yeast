bda541438e461ebf4ae80ebf26b7ade317c9ad6f
Arbona <jeanmichel.arbona@gmail.com>
2017-02-02 14:44:50 +0100

    aading visualisation tools

--- src/data/make_dataset_for_testing_parameters.py
+++ src/data/make_dataset_for_testing_parameters.py
@@ -7,7 +7,7 @@ import json
 if __name__ == "__main__":
     param_file = sys.argv[1]
     parameters = load_parameters(param_file)
-
+    print(sys.argv)
     if len(sys.argv) >= 2:
         parameters["visu"] = True
         if "sumatra_label" in parameters:
