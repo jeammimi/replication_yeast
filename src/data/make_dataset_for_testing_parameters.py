3129b3f63bacdfdedc51a22d11874249adf2c22f
Arbona <jeanmichel.arbona@gmail.com>
2017-02-02 14:43:52 +0100

    aading visualisation tools

--- src/data/make_dataset_for_testing_parameters.py
+++ src/data/make_dataset_for_testing_parameters.py
@@ -2,10 +2,16 @@ import sys
 sys.path.append("./")
 from replication.simulate import simulate, load_parameters
 import os
+import json
 
 if __name__ == "__main__":
     param_file = sys.argv[1]
     parameters = load_parameters(param_file)
+
+    if len(sys.argv) >= 2:
+        parameters["visu"] = True
+        if "sumatra_label" in parameters:
+            parameters.pop("sumatra_label")
 
     if "sumatra_label" in parameters:
         parameters["data_folder"] = os.path.join(parameters["data_folder"],
@@ -13,4 +19,10 @@ if __name__ == "__main__":
         parameters["data_folder"] = os.path.join(parameters["data_folder"], "")
     else:
         print("no extra label")
+
+    parameters["filename"] = param_file
+    with open("params.json", "w") as f:
+        s = json.dumps(parameters)
+        f.write(s)
+
     simulate(parameters)
