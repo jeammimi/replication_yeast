2b3ae8fa466d0b574b94fb14780052cf56628c6f
Arbona <jeanmichel.arbona@gmail.com>
2017-02-16 13:44:37 +0100

    corrected inversed make

--- src/data/make_1D_ensemble_sim.py
+++ src/data/make_1D_ensemble_sim.py
@@ -1,19 +1,18 @@
+
 import sys
 sys.path.append("./")
-from replication.simulate import simulate, load_parameters
+from replication.ensembleSim import ensembleSim
+from replication.simulate import load_parameters
 import os
 import json
-import copy
+import _pickle as cPickle
+import pandas
+
 
 if __name__ == "__main__":
     param_file = sys.argv[1]
     parameters = load_parameters(param_file)
     # print(sys.argv)
-    if len(sys.argv) >= 3:
-        parameters["visu"] = True
-        if "sumatra_label" in parameters:
-            parameters.pop("sumatra_label")
-
     if "sumatra_label" in parameters:
         parameters["data_folder"] = os.path.join(parameters["data_folder"],
                                                  parameters["sumatra_label"])
@@ -28,8 +27,33 @@ if __name__ == "__main__":
         s = json.dumps(parameters)
         f.write(s)
 
-    original = copy.deepcopy(parameters["visu"])
-    parameters["visu"] = True
-    simulate(parameters)  # generate files for visualisation
-    parameters["visu"] = original
-    simulate(parameters)
+    # ensembleSim(Nsim, Nori, Ndiff, lengths, p_on, p_off, only_one,
+    # all_same_ori=False, l_ori=[], cut=10)
+    if type(parameters["Nori"]) == str:
+        Oris = pandas.read_csv(parameters["Nori"], comment="#")
+
+        l_ori = [[] for i in range(16)]
+
+        # Filters ori:
+        for ch, p, status in zip(Oris["chr"], Oris["start"], Oris["status"]):
+            if status in ["Confirmed"]:  # ,"Likely"]:
+                l_ori[ch - 1].append(int(p / 1000))
+
+        # Then remove duplicate (/kb ) and outside of boundaries
+
+        tot = 0
+        for i in range(len(parameters["lengths"])):
+            isize = len(l_ori[i])
+            l_ori[i] = list(set(l_ori[i]))
+            l_ori[i].sort()
+            print(isize, len(l_ori[i]))
+            while not (max(l_ori[i]) < parameters["lengths"][i]):
+                l_ori[i].remove(max(l_ori[i]))
+            # print(max(l_ori[i]),len_chrom[i]*5
+
+            tot += len(l_ori[i])
+            assert(max(l_ori[i]) < parameters["lengths"][i])
+
+    E = ensembleSim(**parameters)
+    with open(os.path.join(parameters["data_folder"], "ensembleSim.pick"), "wb") as f:
+        cPickle.dump(E, f)
