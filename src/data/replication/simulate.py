8751a73f1357c24e23815051ca75bdc64c83484f
Arbona <jeanmichel.arbona@gmail.com>
2017-02-21 11:07:10 +0100

    read data from gff

--- src/data/replication/simulate.py
+++ src/data/replication/simulate.py
@@ -41,6 +41,7 @@ def create_initial_configuration(traj):
                                       traj["ori_type"],
                                       len_chrom,
                                       traj["coarse"])
+    p_ribo = [[position / traj["coarse"], length] for position, length in p_ribo]
 
     # Yeast case
     spb = traj["spb"]
