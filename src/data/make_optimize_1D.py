eafd1184220c6528b11c0c4b59c406e4c30138f4
Arbona <jeanmichel.arbona@gmail.com>
2017-06-29 13:35:10 +0200

    added latin to optimize

--- src/data/make_optimize_1D.py
+++ src/data/make_optimize_1D.py
@@ -151,7 +151,7 @@ if __name__ == "__main__":
         start = [parameters["rNdiff"], parameters["rp_on"]]
     else:
         start = [parameters["rNdiff"], parameters["rp_on"], parameters["rp_off"]]
-    x0 = latin(10, start)
+    x0 = latin(10, start).tolist()
     print(x0)
     res = gp_minimize(error, start, n_jobs=1, n_calls=100, n_random_starts=0, x0=x0)
 
