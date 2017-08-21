14e00d9cd529ce8dbd5512413d7a8312d60802a8
Arbona <jeanmichel.arbona@gmail.com>
2017-08-21 09:41:15 +0200

    fixing pulse

--- src/data/make_1D_ensemble_sim.py
+++ src/data/make_1D_ensemble_sim.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
     if "correlation" in parameters:
         parameters.pop("correlation")
 
-    if "ramp_time" in parameters and parameters.get("ramp", True):
+    if "ramp_time" in parameters and parameters["ramp_time"] is not False:
         parameters["max_ramp"] = parameters["Ndiff"]
         # if parameters["Nori"] == "xenope":
         parameters["ramp"] = parameters["max_ramp"] / parameters.pop("ramp_time")
