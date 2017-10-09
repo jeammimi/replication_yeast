041208f9bbcdfc6c5f61be190e0a52f0efcc1f23
Arbona <jeanmichel.arbona@gmail.com>
2017-10-09 10:13:32 +0200

    CHange default parameters

--- src/data/replication/tools.py
+++ src/data/replication/tools.py
@@ -34,7 +34,7 @@ def load_ori_position(File, ori_type, lengths, coarse, verbose=True, strength=No
         l_ori[i].sort()
         if verbose:
             print(isize, len(l_ori[i]))
-        while not (max(l_ori[i]) <= lengths[i] + 0.99):
+        while not (max(l_ori[i]) <= lengths[i]):
             print("Remave", max(l_ori[i]), lengths[i])
             l_ori[i].remove(max(l_ori[i]))
             strengths[i].pop(-1)
