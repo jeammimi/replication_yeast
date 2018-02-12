c4e40a604a6a0a6287af8d1b085078e2242d6d3c
Arbona <jeanmichel.arbona@gmail.com>
2018-02-12 10:02:45 +0100

    indent parameter

--- src/data/make_3D_simulation.py
+++ src/data/make_3D_simulation.py
@@ -17,12 +17,12 @@ def make_sure_path_exists(path):
 
 if __name__ == "__main__":
     param_file = sys.argv[1]
-
     parameters = load_parameters(param_file)
-    # if len(sys.argv) >= 3:
-    #    parameters["visu"] = True
-    #    if "sumatra_label" in parameters:
-    #        parameters.pop("sumatra_label")
+    # print(sys.argv)
+    if len(sys.argv) >= 3:
+        parameters["visu"] = True
+        if "sumatra_label" in parameters:
+            parameters.pop("sumatra_label")
 
     if "sumatra_label" in parameters:
         parameters["data_folder"] = os.path.join(parameters["data_folder"],
@@ -36,6 +36,7 @@ if __name__ == "__main__":
     print(parameters["data_folder"])
     make_sure_path_exists(parameters["data_folder"])
     with open(os.path.join(parameters["data_folder"], "params.json"), "w") as f:
+        s = json.dumps(parameters, indent=True)
         f.write(s)
 
     if parameters["p_origins"] == "xenope":  # Xenopu
