529f2c914e9fe13548fcbbcc8edbc745baf97fc3
Arbona <jeanmichel.arbona@gmail.com>
2018-02-12 09:49:14 +0100

    indent parameter

--- src/data/make_3D_simulation.py
+++ src/data/make_3D_simulation.py
@@ -17,12 +17,12 @@ def make_sure_path_exists(path):
 
 if __name__ == "__main__":
     param_file = sys.argv[1]
+
     parameters = load_parameters(param_file)
-    # print(sys.argv)
-    if len(sys.argv) >= 3:
-        parameters["visu"] = True
-        if "sumatra_label" in parameters:
-            parameters.pop("sumatra_label")
+    # if len(sys.argv) >= 3:
+    #    parameters["visu"] = True
+    #    if "sumatra_label" in parameters:
+    #        parameters.pop("sumatra_label")
 
     if "sumatra_label" in parameters:
         parameters["data_folder"] = os.path.join(parameters["data_folder"],
